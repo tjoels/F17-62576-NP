629471d7127e7051f4e933a5b7b3eeb97abedfd2
trules <troelsjc@gmail.com>
2017-02-10 12:13:29 +0100

    calculator does +, -, *, /, ^

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double d1, d2;
+    char operator;
+    double result;
+
+    printf("Lommeregner initialiseres. Indtast udregning.\n");
+
+    while (1) {
+        scanf("%lf%c%lf", &d1, &operator, &d2);
+
+        switch (operator) {
+            case '+':
+                result = d1 + d2;
+                break;
+            case '-':
+                result = d1 - d2;
+                break;
+            case '*':
+                result = d1 * d2;
+                break;
+            case '/':
+                result = d1 / d2;
+            case '^':
+                result = pow(d1, d2);
+        }
+
+        printf("%lf %c %lf", d1, operator, d2);
+        printf(" = %lf\n", result);
+    }
+    return 0;
+}
